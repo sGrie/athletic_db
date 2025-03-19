06983626edabd82f339ac6fea7e40ebcadc47af1
Hunter Parcells <hunterparcells@gmail.com>
2025-03-19 12:14:22 -0400

    fix(frontend): Align footer items to top.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className='bg-adb-blue-950 pt-8 pb-8 pl-4 pr-4 flex justify-center items-center flex-wrap gap-4 text-white'>
+    <div className='bg-adb-blue-950 pt-8 pb-8 pl-4 pr-4 flex justify-center items-start flex-wrap gap-4 text-white'>
       <div className='basis-[350] flex flex-col gap-2'>
         <h1 className='text-primary font-extrabold text-2xl'>AthleticDB</h1>
         <p className='text-gray-400 text-sm'>© 2025 All Rights Reserved</p>
