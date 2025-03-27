eb463532ddf538dcd162666e52020cfc06f888cf
Hunter Parcells <hunterparcells@gmail.com>
2025-03-27 14:40:01 -0400

    fix(frontend): Round news image corners.

--- frontend/src/components/NewsCard.tsx
+++ frontend/src/components/NewsCard.tsx
@@ -20,7 +20,7 @@ function NewsCard({
           alt='News Article'
           width={400}
           height={200}
-          className='object-fill w-full'
+          className='object-fill w-full rounded-t-xl'
         />
       </div>
       <CardHeader className='grid gap-1 p-4'>
