3eae7094f0d37dec1d6e6b910e69b068f46206dd
Hunter Parcells <hunterparcells@gmail.com>
2025-03-27 11:59:16 -0400

    feat(frontend): Basic athlete admin panel.

--- frontend/src/components/section/AdminSectionAthletes.tsx
+++ frontend/src/components/section/AdminSectionAthletes.tsx
@@ -1,5 +1,64 @@
-function AdminSectionAthletes() {
-  return <p>TODO</p>;
+import Link from 'next/link';
+import { Pencil, Plus, Trash } from 'lucide-react';
+
+import AthletesTable from '@/components/AthletesTable';
+import DialogAthlete from '@/components/dialog/DialogAthlete';
+import { Button } from '@/components/ui/Button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/Table';
+
+import { getAthletes } from '@/actions/athlete';
+
+async function AdminSectionAthletes() {
+  const athletes = await getAthletes();
+
+  return (
+    <>
+      <DialogAthlete mode='create'>
+        <Button className='mb-4'>
+          <Plus />
+          New Athlete
+        </Button>
+      </DialogAthlete>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className='w-[200px]'>Name</TableHead>
+            <TableHead>School</TableHead>
+            <TableHead className='w-0'>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {athletes.map((athlete) => {
+            return (
+              <TableRow key={athlete.id}>
+                <TableCell>
+                  <Link href={`/athlete/${athlete.id}`}>{athlete.name}</Link>
+                </TableCell>
+                <TableCell>
+                  <Link href={`/school/${athlete.school.slug}`}>{athlete.school.name}</Link>
+                </TableCell>
+                <TableCell className='flex justify-end gap-2'>
+                  <Button size='icon-sm' variant='ghost'>
+                    <Pencil />
+                  </Button>
+                  <Button size='icon-sm' variant='destructive'>
+                    <Trash />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
+  );
 }
 
 export default AdminSectionAthletes;
--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -21,7 +21,8 @@ const buttonVariants = cva(
         default: 'h-9 px-4 py-2',
         sm: 'h-8 px-3 text-xs',
         lg: 'h-10 px-8',
-        icon: 'h-9 w-9'
+        icon: 'h-9 w-9',
+        'icon-sm': 'h-6 w-6 [&_svg]:h-3 [&_svg]:w-3'
       }
     },
     defaultVariants: {
