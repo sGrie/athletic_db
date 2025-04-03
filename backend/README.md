c1d9a9a7e0254f5f2ee7e5290fed5bfdd8fe2100
Hunter Parcells <hunterparcells@gmail.com>
2025-04-03 14:25:27 -0400

    feat(backend): Use psycopg2.

--- backend/README.md
+++ backend/README.md
@@ -3,8 +3,29 @@
 
 ## Installation
 
+Run the following commands.
+
 ```
 py -3 -m venv .venv
 .venv\Scripts\activate
-pip install Flask
+pip install -r requirements.txt
+run.bat
+```
+
+Create a `.env` file with the following fields.
+
+```
+DB_NAME=""
+DB_USERNAME=""
+DB_PASSWORD=""
+```
+
+Update DB information accordingly.
+
+## Useful Python Commands
+
+Freeze all package in `requirements.txt`.
+
+```
+pip freeze > requirements.txt
 ```
--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,26 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, jsonify
+import psycopg2
+
+load_dotenv()
 
 app = Flask(__name__)
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+# TODO: Set up database connection.
+# conn = psycopg2.connect(
+#     dbname="shop",
+#     user="…",
+#     password="…",
+#     host="localhost",
+#     port="5432"
+# )
+# cursor = conn.cursor()
+
 
 @app.route("/")
 def hello_world():
