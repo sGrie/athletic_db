eaf630d329998175ea08d960c26a6f0c89edce9b
Hunter Parcells <hunterparcells@gmail.com>
2025-04-12 12:33:57 -0400

    feat(frontend): All competitions page.

--- frontend/src/app/(p-2)/(centered)/competitions/page.tsx
+++ frontend/src/app/(p-2)/(centered)/competitions/page.tsx
@@ -1,5 +1,21 @@
-function CompetitionsPage() {
-  return <p>Competitions</p>;
+import CompetitionBox from '@/components/CompetitionBox';
+import H2 from '@/components/ui/H2';
+
+import { getCompetitions } from '@/actions/competition';
+
+async function CompetitionsPage() {
+  const competitions = await getCompetitions();
+
+  return (
+    <>
+      <H2>Competitions</H2>
+      <div className='flex flex-col gap-2 pt-2'>
+        {competitions.map((competition) => {
+          return <CompetitionBox competition={competition} key={competition.id} />;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default CompetitionsPage;
